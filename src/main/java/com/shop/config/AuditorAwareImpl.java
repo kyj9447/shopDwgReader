eeceb3ce51db409c7e3aa763f82f65c489ce4488
kyj9447 <kyj9447@gmail.com>
2023-09-20 20:30:01 +0900

    BaseEntity 의 created_By, modified_By에 "이메일/로그인타입"형식으로 저장되게 수정 호출시 OAuth2로 로그인한 경우 이메일이 아닌 계정 고유번호가 저장되는 문제 해결 (일반 계정의 principal.getName()은 이메일이 출력되지만 OAuth2 계정의 principal.getName()은 고유 식별번호가 출력된다)

--- src/main/java/com/shop/config/AuditorAwareImpl.java
+++ src/main/java/com/shop/config/AuditorAwareImpl.java
@@ -1,6 +1,8 @@
 package com.shop.config;
 
+import com.shop.service.AuthTokenParser;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -11,8 +13,14 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = "";
-        if (authentication != null) {
-            userId = authentication.getName();
+        if (authentication != null) { // = principal 있으면
+            if (authentication instanceof AnonymousAuthenticationToken) { // 익명사용자일경우 (ex. 회원가입시 member의 created_by는 익명이다)
+                // 기존 코드
+                userId = authentication.getName();
+            } else {
+                String[] auditor = AuthTokenParser.getParseToken(authentication);
+                userId = auditor[0]+"/"+auditor[1]; // "이메일/type" 형식
+            }
         }
         return Optional.of(userId);
     }
