a7060683746649453184a5b509c706fe8a1ddb00
kyj9447 <kyj9447@gmail.com>
2023-09-20 21:21:05 +0900

    AuthTokenParser isAdmin 메서드 테스트 작성

--- src/main/java/com/shop/service/AuthTokenParser.java
+++ src/main/java/com/shop/service/AuthTokenParser.java
@@ -17,7 +17,7 @@ public class AuthTokenParser {
 
             // role 확인
             parsedToken[1] = authToken.getAuthorities().iterator().next().getAuthority().toLowerCase();
-            ////System.out.println("!OAuth2 role-loginType! : "+parsedToken[1]);
+            //System.out.println("!OAuth2 role-loginType! : "+parsedToken[1]);
 
             // email 확인
             if (parsedToken[1].equals("kakao")) { // 카카오
@@ -25,20 +25,21 @@ public class AuthTokenParser {
             } else { // 구글, 네이버
                 parsedToken[0] = (String) authToken.getPrincipal().getAttributes().get("email");
             }
-            ////System.out.println("!OAuth2 email! : "+parsedToken[0]);
+            //System.out.println("!OAuth2 email! : "+parsedToken[0]);
         } else { //UsernamePasswordAuthenticationToken 사용자 요청이면
             UsernamePasswordAuthenticationToken authToken = (UsernamePasswordAuthenticationToken) principal;
             parsedToken[1] = "normal"; // UsernamePasswordAuthenticationToken 들어오면 모두 normal
-            ////System.out.println("!UsernamePasswordAuth role-loginType! : "+parsedToken[1]);
+            //System.out.println("!UsernamePasswordAuth role-loginType! : "+parsedToken[1]);
 
             parsedToken[0] = (String) authToken.getName();
-            ////System.out.println("!UsernamePasswordAuth email! : "+parsedToken[0]);
+            //System.out.println("!UsernamePasswordAuth email! : "+parsedToken[0]);
         }
 
         return parsedToken;
     }
 
-    public static boolean isAdmin(Principal principal) {
+    public static boolean isAdmin(Principal principal) { // 기존 방식
+
         if (principal instanceof UsernamePasswordAuthenticationToken authToken) { // 일반 사용자 요청이면
             Object[] role = authToken.getAuthorities().toArray();
             String role2 = role[0].toString();
@@ -46,6 +47,20 @@ public class AuthTokenParser {
                 return true;
             }
         }
+
         return false;
     }
+
+    // 새방식 테스트 결과 처리시간 증가 ( 800ns -> 4000ns )
+    // 테스트 필요시 아래 코드 주석 해제
+
+//    public static boolean isAdmin2(Principal principal) { // 새 방식
+//
+//        if (principal instanceof UsernamePasswordAuthenticationToken authToken) {
+//            return authToken.getAuthorities().stream().anyMatch(authority -> "ROLE_ADMIN".equals(authority.getAuthority()));
+//        }
+//
+//        return false;
+//    }
+
 }
---
+++ src/test/java/com/shop/service/AuthTokenParserTest.java
@@ -0,0 +1,152 @@
+package com.shop.service;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.shop.service.AuthTokenParser.isAdmin;
+import static com.shop.service.AuthTokenParser.isAdmin2;
+
+
+@SpringBootTest
+public class AuthTokenParserTest {
+
+    // 테스트를 위한 가짜 Principal 객체 생성
+    private static Principal createTestPrincipal(boolean isAdmin) {
+
+        // 사용자 이름
+        String username = "test";
+
+        String role;
+        // "ROLE_ADMIN" 권한을 가진 SimpleGrantedAuthority 생성
+        if(isAdmin){
+            role = "ROLE_ADMIN";
+        }
+        else {
+            role = "ROLE_USER";
+        }
+        GrantedAuthority adminAuthority = new SimpleGrantedAuthority(role);
+
+        // 권한 목록 생성 및 "ROLE_ADMIN" 권한 추가
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(adminAuthority);
+
+        // UsernamePasswordAuthenticationToken 객체 생성 및 권한 목록 설정
+        // 원하는 Principal 객체를 생성하고 반환
+        return new UsernamePasswordAuthenticationToken(username, "password123", authorities);
+    }
+
+    @Test
+    @DisplayName("토큰테스트 true")
+    public void isAdminTest() {
+        // 테스트를 위한 Principal 객체 생성
+        Principal principal = createTestPrincipal(true);
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin 메서드 실행 시간 측정
+            long startTime = System.nanoTime();
+            boolean isAdminResult = isAdmin(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin 실행 시간 (나노초): " + duration);
+
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin2 메서드 실행 시간 측정
+            long startTime = System.nanoTime();
+            boolean isAdmin2Result = isAdmin2(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin2 실행 시간 (나노초): " + duration);
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin 메서드 실행 시간 재측정
+            long startTime = System.nanoTime();
+            boolean isAdminResult = isAdmin(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin 실행 시간 (나노초): " + duration);
+
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin2 메서드 실행 시간 재측정
+            long startTime = System.nanoTime();
+            boolean isAdmin2Result = isAdmin2(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin2 실행 시간 (나노초): " + duration);
+        }
+    }
+
+    @Test
+    @DisplayName("토큰테스트 false")
+    public void notAdminTest() {
+        // 테스트를 위한 Principal 객체 생성
+        Principal principal = createTestPrincipal(false);
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin 메서드 실행 시간 측정
+            long startTime = System.nanoTime();
+            boolean isAdminResult = isAdmin(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin 실행 시간 (나노초): " + duration);
+
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin2 메서드 실행 시간 측정
+            long startTime = System.nanoTime();
+            boolean isAdmin2Result = isAdmin2(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin2 실행 시간 (나노초): " + duration);
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin 메서드 실행 시간 재측정
+            long startTime = System.nanoTime();
+            boolean isAdminResult = isAdmin(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin 실행 시간 (나노초): " + duration);
+
+        }
+
+        for (int i = 0 ; i<10 ; i++ ) {
+            // isAdmin2 메서드 실행 시간 재측정
+            long startTime = System.nanoTime();
+            boolean isAdmin2Result = isAdmin2(principal);
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+
+            // 결과 출력
+            System.out.println("isAdmin2 실행 시간 (나노초): " + duration);
+        }
+    }
+
+}
